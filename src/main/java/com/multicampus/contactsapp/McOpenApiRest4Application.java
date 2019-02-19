b107a1c48cd066b517ee8482d275f32e64589ae0
JungYoungsu <terun707@naver.com>
2019-02-19 15:05:07 +0900

    START

---
+++ src/main/java/com/multicampus/contactsapp/McOpenApiRest4Application.java
@@ -0,0 +1,13 @@
+package com.multicampus.contactsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McOpenApiRest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McOpenApiRest4Application.class, args);
+	}
+
+}
